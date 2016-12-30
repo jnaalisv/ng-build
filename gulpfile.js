8a4b04880a62ef24f5d1bfc4ee68e76f727074e8
Juho Naalisvaara <juho.naalisvaara@phz.fi>
2016-12-30 10:26:19 +0200

    serve

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+
+var browserSync = require('browser-sync');
+var proxyMiddleware = require('http-proxy-middleware');
+var modRewrite = require('connect-modrewrite');
+
+gulp.task('default', [], function() {
+
+});
+
+gulp.task('serve', [], function() {
+    var middleWares = [];
+
+    var proxyOptions = {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        ws: false,
+        secure: false
+    };
+
+    proxyOptions.target = 'http://localhost:8080';
+
+    middleWares.push(proxyMiddleware('/backend/', proxyOptions));
+    middleWares.push(modRewrite(['^/(student)/[^\\.]*$ /$1/index.html [L]']));
+
+    browserSync.init({
+        notify: false,
+        ui: false,
+        port: 9090,
+        startPath: '/student',
+        server: {
+            baseDir: ['.'],
+            routes: {
+                // '/teacher': '../frontend-teacher/target/classes/dist',
+                '/student': 'webapp',
+                '/student/universityConfig.js': '../config/otm-common/universityConfig.js',
+            },
+            middleware: middleWares
+        }
+    });
+});
