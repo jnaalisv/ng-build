1faf80da5c8fc5711d417acbd87e3b3bf871b65b
Juho Naalisvaara <juho.naalisvaara@phz.fi>
2016-12-30 13:32:39 +0200

    uglify, rev, sourcemaps for dist

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,12 @@ var rev = require('gulp-rev');
 var revReplace = require("gulp-rev-replace");
 var minifyCss = require('gulp-minify-css');
 
+var useref = require('gulp-useref');
+var lazypipe = require('lazypipe');
+var uglify = require('gulp-uglify');
+var ngAnnotate = require("gulp-ng-annotate");
+var filter = require('gulp-filter');
+
 gulp.task('default', [], function() {
 
 });
@@ -25,7 +31,7 @@ gulp.task('clean-dist', [], function () {
 
 var assetsManifestFileName = "assets-manifest.json";
 gulp.task('revision-assets', ['clean-dist'], function() {
-    return gulp.src([webAppDir + "/assets/**"]) // css?
+    return gulp.src([webAppDir + "assets/**"], { base: webAppDir })
         .pipe(rev())
         .pipe(gulp.dest(distDir))
         .pipe(rev.manifest(assetsManifestFileName, {base: process.cwd(), merge: true}))
@@ -46,15 +52,62 @@ gulp.task('less-dist', ['clean-dist'], function() {
 
 });
 
-gulp.task('index-dist', ['revision-assets', 'less-dist'], function() {
+gulp.task('index-dist', ['revision-assets'], function() {
+
+    var jsFilter = filter("**/*.js", {restore: true});
+    var cssFilter = filter("**/*.css", {restore: true});
+    var notIndexFilter = filter(['**/*', '!**/index.html'], {restore: true});
 
     var assetManifests = gulp.src(distDir + assetsManifestFileName);
     var minifiedCssManifest = gulp.src(distDir + cssManifestFileName);
     return gulp.src([webAppDir + 'index.html'])
-        .pipe(revReplace({manifest: assetManifests}))
-        .pipe(revReplace({manifest: minifiedCssManifest}))
+         .pipe(revReplace({manifest: assetManifests}))
+        .pipe(useref({}, lazypipe().pipe(sourcemaps.init, { loadMaps: true })))
+        .pipe(jsFilter)
+        .pipe(ngAnnotate())
+        .pipe(uglify())
+        .pipe(jsFilter.restore)
+        .pipe(cssFilter)
+        .pipe(less())
+        .pipe(cssFilter.restore)
+        .pipe(notIndexFilter)
+        .pipe(rev())
+        .pipe(notIndexFilter.restore)
+        .pipe(revReplace())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(distDir));
+});
 
+gulp.task('serve-dist', ['index-dist'], function() {
+    var middleWares = [];
+
+    var proxyOptions = {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        ws: false,
+        secure: false
+    };
+
+    proxyOptions.target = 'http://localhost:8080';
+
+    middleWares.push(proxyMiddleware('/backend/', proxyOptions));
+    middleWares.push(modRewrite(['^/(student)/[^\\.]*$ /$1/index.html [L]']));
+
+    browserSync.init({
+        notify: false,
+        ui: false,
+        port: 9090,
+        startPath: '/student',
+        server: {
+            baseDir: ['.'],
+            routes: {
+                // '/teacher': '../frontend-teacher/target/classes/dist',
+                '/student': 'dist',
+                '/student/universityConfig.js': '../config/otm-common/universityConfig.js',
+            },
+            middleware: middleWares
+        }
+    });
 });
 
 gulp.task('less-dev', [], function() {
