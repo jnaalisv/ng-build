13c0e6435ed176832b221f47d0b247768e28857f
Juho Naalisvaara <juho.naalisvaara@phz.fi>
2017-01-04 13:43:06 +0200

    add source file to stream

---
+++ version.js
@@ -0,0 +1,8 @@
+'use strict';
+angular
+    .module('version', [])
+    .constant('FRONTEND_VERSION', {
+        'BUILD_DATE': 'Mon Jan 02 2017 09:46:21 GMT+0200 (EET)',
+        'BUILD_VERSION': 'v0.8.0-rc1-1307-gab5202dd7',
+        'JENKINS_BUILD_NUMBER': 'undefined'
+    });
--- webapp/app/angularApp.js
+++ webapp/app/angularApp.js
@@ -2,13 +2,17 @@
     'use strict';
 
     angular
-        .module('osuva', ['commonLib', 'osuva.directives'])
+        .module('osuva', ['commonLib', 'osuva.directives', 'version'])
         .controller('AppCtrl', AppCtrl);
 
-    function AppCtrl(commonService) {
+    function AppCtrl(commonService, FRONTEND_VERSION) {
         var vm = this;
 
         vm.message = 'this is a message from your controller';
+
+        vm.FRONTEND_VERSION = FRONTEND_VERSION.BUILD_VERSION;
+        vm.BUILD_DATE = FRONTEND_VERSION.BUILD_DATE;
+
         commonService.helloWorld();
     }
 
